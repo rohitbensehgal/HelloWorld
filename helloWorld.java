485f67691166f3bda1f689d528fbefaf5228c9ea
rohitbensehgal <31272706+rohitbensehgal@users.noreply.github.com>
2017-08-23 03:17:28 -0500

    Update helloWorld.java
    using no strings lol

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,5 @@
 import java.util.Arrays;
-//hello world using no integers;
+//hello world using no strings lol;
 public class helo {
 	public static void main(String[] args) {
 		int[] helloLetters = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 32};
